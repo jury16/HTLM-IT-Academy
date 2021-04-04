056e6afc81f2ce8a7685f7dd3aa36ef9a63a0b17
Yurii <76671367+jury16@users.noreply.github.com>
2021-04-03 23:07:33 -0400

    Add files for task #38

---
+++ script.js
@@ -0,0 +1,20 @@
+//console.log("Привет из script.js");
+
+function setCountry() {
+    let countryName = prompt('Input name of the country');
+    let capitalName = prompt('Input name of the capital');
+    addСountry(countryName, capitalName);
+}
+
+function getCountry(){
+    let countryName = prompt('Input name of the country');
+    console.log(getcountryInfo(countryName));
+}
+
+function registerCountries() {
+    console.log(listСountries());
+}
+function clearCountry() {
+    let countryName = prompt('Input name of the country to delete');
+    deleteСountry(countryName);
+}
